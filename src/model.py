1ba502041ca3614d169d7c9f98edfae84a69f4cf
Zeeshan Waris <zeeshhanwaris3345@example.com>
2026-07-16 18:01:25 +0500

    Adding the Custom Dataset Class in the model.py for the transformation of training and validation datasets

--- src/model.py
+++ src/model.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torch import nn
+from PIL import Image
+
 
 label_columns = [
     "No Finding",
@@ -32,5 +37,27 @@ validation_df[label_columns] = validation_df[label_columns].fillna(0)
 train_df[label_columns] = train_df[label_columns].replace(-1, 0)
 validation_df[label_columns] = validation_df[label_columns].replace(-1, 0)
 
-print(train_df.head())
-print(train_df.info())
+class ChestXrayDataset(Dataset):
+    def __init__(self,dataframe,root_dir,label_cols,transforms = None):
+        self.dataframe = dataframe
+        self.root_dir = root_dir
+        self.label_cols = label_cols
+        self.transform = transforms
+
+    def __len__(self):
+        return len(self.dataframe)
+    
+    def __getitem__(self, key):
+        row = self.dataframe.iloc[key]
+        image_path = self.root_dir / row["Path"]
+        image = Image.open(image_path).convert("RGB")
+        
+        if self.transform:
+            image = self.transform(image)
+
+        labels = row[self.label_cols].values.astype(np.float32)
+        labels = torch.tensor(labels)
+
+        return image,labels
+    
+
