2b0a89d1c8ebc3ea204099cadea80beaa6f723cb
Zeeshan Waris <zeeshanwaris3345@gmail.com>
2026-07-17 06:19:53 +0500

    Added the main endpoint of the backend which receives the image and returns the result back to the frontend

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI,File,UploadFile
+from PIL import Image
+from inference import predict
+
+app = FastAPI()
+
+@app.get("/")
+def Home():
+    return {"message": "Chest Disease X-Ray API is running"}
+
+@app.post("/predict")
+async def predict_image(file: UploadFile = File(...)):
+    # Read the uploaded image
+    image = Image.open(file.file)
+    
+    # Perform prediction
+    result = predict(image)
+    
+    return {"prediction": result}
